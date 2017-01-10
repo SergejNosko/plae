cf9102907876ce6269f26da7937ca0808b224285
SergejNosko <tuzemec81@gmail.com>
2017-01-10 19:27:59 +0200

    finish styles of Why-block, not finished hover-effect on the rounds

--- source/js/app.js
+++ source/js/app.js
@@ -1,5 +1,5 @@
 var main = function(){
-
+//Slider
 	$('.arrow-right').click(function(){
 		var currentSlide = $('.active-slide');
 		var nextSlide = $('.active-slide').next();
@@ -11,17 +11,72 @@ var main = function(){
 		nextSlide.fadeIn(300).addClass('active-slide');
 	});
 
-$('.arrow-left').click(function(){
-		var currentSlide = $('.active-slide');
-		var prevSlide = $('.active-slide').prev();
+	$('.arrow-left').click(function(){
+			var currentSlide = $('.active-slide');
+			var prevSlide = $('.active-slide').prev();
 
-		if(prevSlide.length == 0){
-			prevSlide = $(".slider-item").last();
+			if(prevSlide.length == 0){
+				prevSlide = $(".slider-item").last();
+			}
+			currentSlide.fadeOut(10).removeClass('active-slide');
+			prevSlide.fadeIn(300).addClass('active-slide');
+		});
+
+//Why-block hover
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	/*var boxes = [], rounds = [];
+	for(var i = 0; i < 9; i++){
+		var b = $('.shoe-feature:eq(' + i + ')'), r = $('.round:eq(' + i + ')');
+		boxes.push(b);
+		rounds.push(r);
+	}
+	for(var i = 0; i < 9; i++){
+		boxes[i].hover(function(){
+			rounds[i].style.backgroundImage = 'url(images/why-round-back-hover.png)';//css('background-image', 'url(images/why-round-back-hover.png)');
+		}, function() {
+			rounds.style.backgroundImage = 'url(images/why-round-back.png)';//css('background-image', 'url(images/why-round-back.png)');
+		})
+	}
+//		 = $('.shoe-feature:eq(' + i + ')'),  = $('.round:eq(' + i + ')');
+		/*boxes.hover(function () {
+			rounds.css('background-image', 'url(images/why-round-back-hover.png)');
+		/*.animate({
+				backgroundImage : 'images/why-round-back-hover.png'
+			}, 500);
+		}, function () {
+			rounds.animate({
+				backgroundImage: 'images/why-round-back.png'
+			}, 500);
+		}, function(){
+			rounds.css('background-image', 'url(images/why-round-back.png)');
 		}
-		currentSlide.fadeOut(10).removeClass('active-slide');
-		prevSlide.fadeIn(300).addClass('active-slide');
-	});
+	);*/
 };
 
+	
 
 $(document).ready(main);
