b42aad954173b6d9ed6e8aad09447afbbaf07006
SergejNosko <tuzemec81@gmail.com>
2017-01-11 14:13:24 +0200

    finish an animation of Why-block

--- source/js/app.js
+++ source/js/app.js
@@ -23,58 +23,14 @@ var main = function(){
 		});
 
 //Why-block hover
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	/*var boxes = [], rounds = [];
-	for(var i = 0; i < 9; i++){
-		var b = $('.shoe-feature:eq(' + i + ')'), r = $('.round:eq(' + i + ')');
-		boxes.push(b);
-		rounds.push(r);
-	}
-	for(var i = 0; i < 9; i++){
-		boxes[i].hover(function(){
-			rounds[i].style.backgroundImage = 'url(images/why-round-back-hover.png)';//css('background-image', 'url(images/why-round-back-hover.png)');
-		}, function() {
-			rounds.style.backgroundImage = 'url(images/why-round-back.png)';//css('background-image', 'url(images/why-round-back.png)');
-		})
-	}
-//		 = $('.shoe-feature:eq(' + i + ')'),  = $('.round:eq(' + i + ')');
-		/*boxes.hover(function () {
-			rounds.css('background-image', 'url(images/why-round-back-hover.png)');
-		/*.animate({
-				backgroundImage : 'images/why-round-back-hover.png'
-			}, 500);
-		}, function () {
-			rounds.animate({
-				backgroundImage: 'images/why-round-back.png'
-			}, 500);
+	$('.shoe-feature').hover(function(){
+			var i = $(this).attr("num");
+			$('.round-' + i).css('background-image', 'url(images/why-round-back-hover.png)');
 		}, function(){
-			rounds.css('background-image', 'url(images/why-round-back.png)');
+			var i = $(this).attr("num");
+			$('.round-' + i).css('background-image', 'url(images/why-round-back.png)');
 		}
-	);*/
+	);
 };
 
 	
