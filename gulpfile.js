22e87f371d162d8c0d4e81833db75dec9b9e300a
SergejNosko <tuzemec81@gmail.com>
2017-02-10 16:43:09 +0200

    gulp images

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,25 @@
-var gulp = required('gulp');
+
+
+var gulp = require('gulp'),
+		newer = require('gulp-newer'),
+		imagemin = require('gulp-imagemin');
 
 
 var source = 'source/',
-		dest = 'build/';
+		dest = 'build/',
+		images = {
+			in: source + "/images/*.*",
+			out: dest + "/images/*.*"
+		}
 
+		;
 
+gulp.task('images', function(){
+	return gulp.src(images.in)
+	.pipe(newer(images.out))
+	.pipe(imagemin())
+	.pipe(gulp.dest(images.out));
+});
 
 gulp.task('default', function(){
 		
